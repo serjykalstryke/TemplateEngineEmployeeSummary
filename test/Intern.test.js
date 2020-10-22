fcb57dc8286812ac69d0779cee21307e47d36089
David Stinnett <davidstinnett@icloud.com>
2020-10-22 14:24:15 -0400

    initial upload

---
+++ test/Intern.test.js
@@ -0,0 +1,19 @@
+const Intern = require("../libraries/intern");
+
+test("Can set school from constructor function", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+  const testValue = "Intern";
+  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get school from getSchool()", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
