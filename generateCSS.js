bddd33901c434c13693ad94ab57a61a32dc34fbe
David Stinnett <davidstinnett@icloud.com>
2020-10-22 15:32:15 -0400

    moved .json files to dependencies folder for better application organization

--- generateCSS.js
+++ generateCSS.js
@@ -1,36 +1,39 @@
 function generateCSS() {
-    return `.card { 
-        margin: auto;
-        width: 100%
-    }
-    
-    .card-header {
-            background-color:  #2C001E;
-            color: white
-        }
-    
-        .card-body{
-            background: #AEA79F;
+    return `
+    .card { 
             margin: auto;
+            width: 100%;
+            border: 0px;
             width: 100%
         }
-    
-        .card-columns {
-            @include media-breakpoint-only(lg) {
-                column-count: 4;
+        
+        .card-header {
+                background-color:  #2C001E;
+                color: white;
+                border: 0px;
+                width: 100%
             }
-        }
-    
-        .main {
-            background: #5E2750;
-            border-radius: 10%
-        }
-    
-    html,
-    body {
-                margin: 20px;
-                background: #E95420
-            }`
+        
+            .card-body{
+                background: #AEA79F;
+                margin: auto;
+                width: 100%;
+            }
+        
+            .card-columns {
+                @include media-breakpoint-only(lg) {
+                    column-count: 4;
+                }
+            }
+        
+            .main {
+                background: #5E2750;
+            }
+        
+        html,
+        body {
+                    background: #5E2750;
+                }`
 
 }
 
